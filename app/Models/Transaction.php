0ba12b26b53e75f88626e3fdf8abc89e7d670805
Azrul Haifan <ahaifan@gmail.com>
2025-03-02 20:21:29 +0700

    Make sure to update account balance when transaction updated

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -53,6 +53,10 @@ class Transaction extends Model
             $transaction->updateAccountBalances();
         });
 
+        static::updated(function (Transaction $transaction) {
+            $transaction->updateAccountBalancesOnUpdate();
+        });
+
         static::deleted(function (Transaction $transaction) {
             $transaction->revertAccountBalances();
         });
@@ -78,23 +82,57 @@ class Transaction extends Model
         }
     }
 
-    protected function revertAccountBalances(): void
+    protected function updateAccountBalancesOnUpdate(): void
     {
-        $amount = $this->amount;
+        // Jika tipe transaksi berubah, kita perlu mengembalikan saldo lama dan menerapkan saldo baru
+        if ($this->wasChanged('type') || $this->wasChanged('account_id') || $this->wasChanged('to_account_id')) {
+            // Revert old transaction
+            $oldType = $this->getOriginal('type');
+            $oldAmount = $this->getOriginal('amount');
+            $oldAccountId = $this->getOriginal('account_id');
+            $oldToAccountId = $this->getOriginal('to_account_id');
 
-        switch ($this->type) {
-            case 'income':
-                $this->account->decrement('current_balance', $amount);
-                break;
+            $oldAccount = Account::find($oldAccountId);
+            $oldToAccount = $oldToAccountId ? Account::find($oldToAccountId) : null;
 
-            case 'expense':
-                $this->account->increment('current_balance', $amount);
-                break;
+            if ($oldAccount) {
+                switch ($oldType) {
+                    case 'income':
+                        $oldAccount->decrement('current_balance', $oldAmount);
+                        break;
+                    case 'expense':
+                        $oldAccount->increment('current_balance', $oldAmount);
+                        break;
+                    case 'transfer':
+                        if ($oldToAccount) {
+                            $oldAccount->increment('current_balance', $oldAmount);
+                            $oldToAccount->decrement('current_balance', $oldAmount);
+                        }
+                        break;
+                }
+            }
 
-            case 'transfer':
-                $this->account->increment('current_balance', $amount);
-                $this->toAccount->decrement('current_balance', $amount);
-                break;
+            // Apply new transaction
+            $this->updateAccountBalances();
+        }
+        // Jika hanya jumlah yang berubah, kita cukup menerapkan selisih
+        elseif ($this->wasChanged('amount')) {
+            $oldAmount = $this->getOriginal('amount');
+            $newAmount = $this->amount;
+            $difference = $newAmount - $oldAmount;
+
+            switch ($this->type) {
+                case 'income':
+                    $this->account->increment('current_balance', $difference);
+                    break;
+                case 'expense':
+                    $this->account->decrement('current_balance', $difference);
+                    break;
+                case 'transfer':
+                    $this->account->decrement('current_balance', $difference);
+                    $this->toAccount->increment('current_balance', $difference);
+                    break;
+            }
         }
     }
 }
