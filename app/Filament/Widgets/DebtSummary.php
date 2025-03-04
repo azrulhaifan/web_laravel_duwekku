0c1f085b681a449b99a6fce68e74d36f54584aef
Azrul Haifan <ahaifan@gmail.com>
2025-03-04 23:57:45 +0700

    Update Dashboard widget

--- app/Filament/Widgets/DebtSummary.php
+++ app/Filament/Widgets/DebtSummary.php
@@ -9,19 +9,19 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 class DebtSummary extends BaseWidget
 {
     protected static ?int $sort = 3;
-    
+
     protected function getStats(): array
     {
         // Get total payable (hutang)
         $totalPayable = Debt::where('type', 'payable')
             ->where('is_settled', false)
             ->sum('amount');
-            
+
         // Get total receivable (piutang)
         $totalReceivable = Debt::where('type', 'receivable')
             ->where('is_settled', false)
             ->sum('amount');
-            
+
         // Get count of upcoming due debts
         $upcomingDueCount = Debt::where('is_settled', false)
             ->whereNotNull('due_date')
@@ -29,16 +29,16 @@ class DebtSummary extends BaseWidget
             ->count();
 
         return [
-            Stat::make('Total Hutang', 'Rp ' . number_format($totalPayable, 0, ',', '.'))
+            Stat::make('Total Hutang (Harus Dibayar)', 'Rp ' . number_format($totalPayable, 0, ',', '.'))
                 ->description('Belum lunas')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('danger'),
-                
-            Stat::make('Total Piutang', 'Rp ' . number_format($totalReceivable, 0, ',', '.'))
+
+            Stat::make('Total Piutang (Uang Kembali)', 'Rp ' . number_format($totalReceivable, 0, ',', '.'))
                 ->description('Belum lunas')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('success'),
-                
+
             Stat::make('Jatuh Tempo Dalam 7 Hari', $upcomingDueCount)
                 ->description('Hutang & Piutang')
                 ->descriptionIcon('heroicon-m-clock')
--- app/Filament/Widgets/MonthlyIncomeExpense.php
+++ app/Filament/Widgets/MonthlyIncomeExpense.php
@@ -10,38 +10,38 @@ use Illuminate\Support\Facades\DB;
 class MonthlyIncomeExpense extends ChartWidget
 {
     protected static ?string $heading = 'Pemasukan & Pengeluaran Bulanan';
-    
+
     protected static ?int $sort = 5;
-    
+
     protected int | string | array $columnSpan = 'full';
-    
+
     protected function getData(): array
     {
         $months = collect();
         $incomes = collect();
         $expenses = collect();
-        
+
         // Get data for the last 6 months
         for ($i = 5; $i >= 0; $i--) {
             $date = Carbon::now()->subMonths($i);
             $monthLabel = $date->format('M Y');
-            
+
             $months->push($monthLabel);
-            
+
             $monthlyIncome = Transaction::where('type', 'income')
                 ->whereMonth('date', $date->month)
                 ->whereYear('date', $date->year)
                 ->sum('amount');
-                
+
             $monthlyExpense = Transaction::where('type', 'expense')
                 ->whereMonth('date', $date->month)
                 ->whereYear('date', $date->year)
                 ->sum('amount');
-                
+
             $incomes->push($monthlyIncome);
             $expenses->push($monthlyExpense);
         }
-        
+
         return [
             'datasets' => [
                 [
@@ -60,9 +60,9 @@ class MonthlyIncomeExpense extends ChartWidget
             'labels' => $months->toArray(),
         ];
     }
-    
+
     protected function getType(): string
     {
-        return 'bar';
+        return 'line';
     }
 }
